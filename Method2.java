c20f79270804dd989ca8812f543551d67a023be1
valeryn <sergejevs.valerijs>
2022-06-12 12:05:10 +0300

    [feature]: Average.java file update.

---
+++ Method2.java
@@ -0,0 +1,13 @@
+package homeworkTwo;
+
+public class Method2 {
+    static void printBusinessCardTwo(String name, String surname, String telephone, int birth){
+        System.out.println("Vizitkarte");
+        System.out.println("########");
+        System.out.println("Vards:" + name);
+        System.out.println("Uzvards:" + surname);
+        System.out.println("Telefon:" + telephone);
+        System.out.println("Dzimsanas gads:" + birth);
+        System.out.println("########");
+    }
+}
